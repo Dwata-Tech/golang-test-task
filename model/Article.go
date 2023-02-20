011aa170f249b7bd2f4937a60bf1ad37be2f6c68
Vaibhav Bhosle <vaibhav@dwatatech.com>
2023-02-20 19:30:11 +0530

    working with gorm model

--- model/Article.go
+++ model/Article.go
@@ -1,20 +1,11 @@
 package model
 
-import (
-	"gorm.io/gorm"
-	"time"
-)
+import "gorm.io/gorm"
 
 type Article struct {
-	ID        uint      `gorm:"primary_key" json:"id"`
-	CreatedAt time.Time `json:"createdAt"`
-	Nickname  string    `json:"nickname"`
-	Title     string    `json:"title"`
-	Content   string    `json:"content"`
-	Comments  []Comment `json:"comments"`
-}
-
-func (m *Article) BeforeCreate(tx *gorm.DB) error {
-	m.CreatedAt = time.Now()
-	return nil
+	gorm.Model
+	Nickname string    `json:"nickname"`
+	Title    string    `json:"title"`
+	Content  string    `json:"content"`
+	Comments []Comment `json:"comments"`
 }
--- model/Comment.go
+++ model/Comment.go
@@ -1,20 +1,11 @@
 package model
 
-import (
-	"gorm.io/gorm"
-	"time"
-)
+import "gorm.io/gorm"
 
 // Comment represents a database model for a comment on an article
 type Comment struct {
-	ID        uint      `gorm:"primary_key" json:"id"`
-	CreatedAt time.Time `json:"createdAt"`
-	Nickname  string    `json:"nickname"`
-	Content   string    `json:"content"`
-	ArticleID uint      `json:"articleID"`
-}
-
-func (m *Comment) BeforeCreate(tx *gorm.DB) error {
-	m.CreatedAt = time.Now()
-	return nil
+	gorm.Model
+	Nickname  string `json:"nickname"`
+	Content   string `json:"content"`
+	ArticleID uint   `json:"articleID"`
 }
